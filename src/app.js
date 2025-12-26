8b99d266b07f98fdb05400f1f8bf5d5b90780a14
Nishant Ranjan <nishantranjan2105@gmail.com>
2025-12-26 14:13:31 +0530

    Discovered about multiple Routes

--- src/app.js
+++ src/app.js
@@ -2,46 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user/:userId/:password",(req,res) => {
-    console.log(req.params);
-    res.send("This is the user page");
-})
-
-app.get("/ab*c", (req,res) => {
-    res.send("this is abc");
-});
-
-app.get("/happy", (req,res) => {
-    res.send("This is the get response to Happy!!!!!");
-});
-
-// app.use("/happy", (req,res) => {
-//     res.send("happyyyyy:)")
-// })
-
-app.post("/happy", (req,res) => {
-    res.send("This is the post response to happy!!!!!");
-});
-
-app.delete("/happy", (req,res) => {
-    res.send("This is the delete response to happy!!!!!");
-});
-
-app.use("/contact", (req,res) => {
-    res.send("Hello from the contact");
-});
-
-app.use("/contact/Mohit", (req,res) =>{
-    res.send("Contacting to Mohit")
-}),
-
-app.use("/test", (req,res) => {
-    res.send("Hello from the test");
-}),
-
-app.use("/", (req,res) => {
-    res.send("Hello from the dashboard");
-});
+app.use("/user", (req,res,next) => {
+    console.log("Route 1");
+    // res.send("Hi this is 1st route");
+    next();
+},
+(req,res,next)=>{
+    console.log("Route 2");
+   
+    next();
+    //  res.send("Hi this is 2nd route");
+     console.log("huh!!")
+},
+(req,res) => {
+    console.log("Route 3");
+    // res.send("Hi this is 3rd route");
+}
+)
 
 app.listen(3000, () => {
     console.log("The server has been established");
