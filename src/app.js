ee4eafe503efc866ff924edc9d57ec904ce10608
Nishant Ranjan <nishantranjan2105@gmail.com>
2025-12-26 13:53:05 +0530

    Understood about routes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,37 @@ const express = require('express');
 
 const app = express();
 
+app.get("/user/:userId/:password",(req,res) => {
+    console.log(req.params);
+    res.send("This is the user page");
+})
+
+app.get("/ab*c", (req,res) => {
+    res.send("this is abc");
+});
+
+app.get("/happy", (req,res) => {
+    res.send("This is the get response to Happy!!!!!");
+});
+
+// app.use("/happy", (req,res) => {
+//     res.send("happyyyyy:)")
+// })
+
+app.post("/happy", (req,res) => {
+    res.send("This is the post response to happy!!!!!");
+});
+
+app.delete("/happy", (req,res) => {
+    res.send("This is the delete response to happy!!!!!");
+});
+
 app.use("/contact", (req,res) => {
     res.send("Hello from the contact");
+});
+
+app.use("/contact/Mohit", (req,res) =>{
+    res.send("Contacting to Mohit")
 }),
 
 app.use("/test", (req,res) => {
