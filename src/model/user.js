a62ae23c3a424488bb393bfd9a0f05ff35e168d3
Nishant Ranjan <nishantranjan2105@gmail.com>
2026-01-04 22:45:58 +0530

    schema validations

--- src/model/user.js
+++ src/model/user.js
@@ -1,25 +1,63 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: [18, "Age must be atleast 18"],
+    },
+    gender: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("This gender dont exist");
+        }
+      },
+    },
+    photoURL: {
+      type: String,
+      default:
+        "https://i0.wp.com/e-quester.com/wp-content/uploads/2021/11/placeholder-image-person-jpg.jpg?fit=820%2C678&ssl=1",
+    },
+    about: {
+      type: String,
+      default: "Hey I am a dev",
+    },
+    skills: {
+      type: [String],
+      required: true,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("user", userSchema);
 
 module.exports = {
-    User
+  User,
 };
